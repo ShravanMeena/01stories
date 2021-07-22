9c8ea6e5be48795355046d4f3f58c529c9d2f434
Shravan <41790742+ShravanMeena@users.noreply.github.com>
2021-07-22 22:17:19 +0530

    Update TagItem.js

--- components/tag/TagItem.js
+++ components/tag/TagItem.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Tag, Text } from "../../pages/Home.style";
 
 export default function TagItem(props) {
-  const { title, description, item } = props;
+  const { title, description, item,key } = props;
   return (
-    <Tag>
+    <Tag key={key}>
       <Text>{item}</Text>
     </Tag>
   );
